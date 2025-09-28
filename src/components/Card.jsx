366ec082658769f6f83dcb822abea0a6b5c058bd
Ritesh Sharma <rtskrm@gmail.com>
2025-09-28 22:22:08 +0530

    add Card component and integrate it into About page

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Card() {
+  return (
+    <div>
+      <h2>Card Title</h2>
+      <p>This is a description of the card.</p> 
+        <button>Click Me</button>
+    </div>
+  )
+}
+
+export default Card
